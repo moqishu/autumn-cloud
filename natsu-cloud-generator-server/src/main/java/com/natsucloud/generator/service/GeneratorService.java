2593ab69f13be1ef8f10a28980318b24f6ab07a3
wenlong lin <lmnlwl@163.com>
2020-04-09 17:47:39 +0800

    代码自动生成

--- natsu-cloud-generator-server/src/main/java/com/natsucloud/generator/service/GeneratorService.java
+++ natsu-cloud-generator-server/src/main/java/com/natsucloud/generator/service/GeneratorService.java
@@ -1,7 +1,11 @@
 package com.natsucloud.generator.service;
 
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.AbstractTemplateEngine;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+import sun.security.krb5.internal.crypto.Des;
 
 /**
  * 代码生成器
@@ -19,6 +23,47 @@ public class GeneratorService {
         gc.setOpen(false);
         gc.setSwagger2(true);
         mpg.setGlobalConfig(gc);
+
+        // 数据库配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(generateConfig.getDbType());
+        dsc.setDriverName(generateConfig.getJdbcDriver());
+        dsc.setUrl(generateConfig.getJdbcUrl());
+        dsc.setUsername(generateConfig.getJdbcUserName());
+        dsc.setPassword(generateConfig.getJdbcPassword());
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(generateConfig.getModuleName());
+        pc.setParent(generateConfig.getParentPackage());
+        //实体类父包
+        pc.setEntity("client.model.entity");
+        //controller父包
+        pc.setController("server.controller");
+        //mapper父包
+        pc.setMapper("server.mapper");
+        //xml父包
+        pc.setXml("server.xml");
+        pc.setServiceImpl("server.service.impl");
+        pc.setService("server.service");
+        mpg.setPackageInfo(pc);
+
+        // 配置模板
+        TemplateConfig tmp = new TemplateConfig();
+        mpg.setTemplateEngine(new VelocityTemplateEngine());
+        mpg.setTemplate(tmp);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+
+
+
+        // 执行生成
+        mpg.execute();
+
     }
 
 }
