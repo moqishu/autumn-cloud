0458c4a8dde4956acf47ae24e554134349e3331e
Administrator <lmnlwl@163.com>
2020-04-20 07:34:05 +0800

    删除.idea

--- natsu-cloud-base-server/src/main/java/com/natsucloud/NatsuCloudBaseServerApplication.java
+++ natsu-cloud-base-server/src/main/java/com/natsucloud/NatsuCloudBaseServerApplication.java
@@ -2,8 +2,10 @@ package com.natsucloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NatsuCloudBaseServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NatsuCloudBaseServerApplication.class, args);
