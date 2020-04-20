d0cd0d535be3b27103b940826dfb5b5886604487
wenlong lin <linwenlong@rd.keytop.com.cn>
2020-04-20 16:26:01 +0800

    网关服务

---
+++ natsu-cloud-zuul-server/src/main/java/com/natsucloud/zuul/NatsuCloudZuulApplication.java
@@ -0,0 +1,18 @@
+package com.natsucloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableZuulProxy
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NatsuCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatsuCloudZuulApplication.class,args);
+    }
+}
